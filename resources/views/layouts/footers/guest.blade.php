d5d047281a19ff9f2c17350fb9bb765c742056d4
Zwel Linn Htet Aung <zwelinnhtetag@gmail.com>
2021-01-27 21:49:31 +0630

    Initial Commit

---
+++ resources/views/layouts/footers/guest.blade.php
@@ -0,0 +1,18 @@
+<footer class="footer">
+    <div class="container">
+        <nav class="float-left">
+        <ul>
+            <li>
+                &copy;
+                <script>
+                    document.write(new Date().getFullYear())
+                </script>, for better Web
+            </li>
+        </ul>
+        </nav>
+        <div class="copyright float-right">
+         made with <i class="material-icons">favorite</i> by
+        <a href="" target="_blank"> ZLHAg </a>
+        </div>
+    </div>
+</footer>
